bc33dd0706da6da56ab8d7f6ee94415385023c86
Harshil Maniar <maniarharshil1000@gmail.com>
2021-05-12 13:52:19 +0530

    version 1.0

---
+++ index.js
@@ -0,0 +1,117 @@
+const cron = require("node-cron");
+const axios = require("axios").default;
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const baseUrl =
+  "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict";
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+cron.schedule("*/30 * * * *", () => {
+  const date = new Date();
+  let availableCenters = [];
+  axios
+    .get(baseUrl, {
+      params: {
+        district_id: 770,
+        date:
+          date.getDate() +
+          "-" +
+          (parseInt(date.getMonth(), 10) + 1) +
+          "-" +
+          date.getFullYear(),
+      },
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+      },
+    })
+    .then((response) => response.data)
+    .then(({ centers }) => {
+      centers.map((center) => {
+        let availableDate = [];
+        center.sessions.map((session) => {
+          if (session.available_capacity > 0 && session.min_age_limit) {
+            availableDate.push(session.date);
+          }
+          if (availableDate.length > 0)
+            availableCenters.push({
+              centerName: center.name,
+              centerAddress: center.address,
+              centerPincode: center.pincode,
+              fee: center.fee_type === "Free" ? 0 : center.vaccine_fees[0].fee,
+              datesAvailable: availableDate,
+            });
+        });
+      });
+
+      if (availableCenters.length > 0) {
+        let head = `
+      <html>
+        <head>
+          <style>
+            table {
+              border: 1px solid;
+              } 
+            td, th{
+              padding: 5px;
+              border: 1px solid;
+            } 
+          </style>
+        </head>
+        `;
+        let tableRows = availableCenters.map(
+          (center) =>
+            `
+          <tr>
+            <td>${center.centerName}</td>
+            <td>${center.centerAddress}</td>
+            <td>${center.centerPincode}</td>
+            <td>${center.fee}</td>
+            <td>${center.datesAvailable}</td>
+          </tr>
+          `
+        );
+        tableRows = tableRows.join("");
+        text =
+          head +
+          "<body> <table> <thead> <tr> <th>Name</th><th>Address</th><th>Pincode</th><th>Fees</th><th>Dates available</th></tr></thead>" +
+          tableRows +
+          "</table></body></html>";
+        const mailOptions = {
+          from: "harshilmaniar.app@gmail.com",
+          to: "maniarharshil1000@gmail.com",
+          subject:
+            "Covid vaccine update for " +
+            date.getHours() +
+            ":" +
+            date.getMinutes(),
+          html: text,
+          headers: {
+            priority: "high",
+          },
+        };
+
+        transporter.sendMail(mailOptions, function (err, info) {
+          if (err) console.log(err);
+          else console.log(info);
+        });
+      } else {
+        console.log(
+          "No vaccine slots found for 18+ . CRON job run time ",
+          date
+        );
+      }
+    })
+    .catch((error) => console.log(error));
+});
